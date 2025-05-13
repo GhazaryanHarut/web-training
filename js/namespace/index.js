202cfae112e96a19493fb5f2996a49f0da6d1953
GhazaryanHarut <mecnharut11@gmail.com>
2025-05-13 22:23:31 +0400

    Update index.js

--- js/namespace/index.js
+++ js/namespace/index.js
@@ -1,12 +1,8 @@
 const obj =namespace('a.b.c.d.e')
-console.log(obj); // {"a":{"b":{"c":{"d":{"e":{}}}}}}
-console.log(obj.a.b.c); // { d: { e: {} } }
 function namespace(str) {
     const parts = str.split('.');
-    console.log(parts);
     let result = {};
     const recursive = (obj, parts) => {
-        console.log(obj, parts);
         if (parts.length === 0) return obj;
         const part = parts.shift();
         obj[part] = {};
